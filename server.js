947f3cc636a22c53252c1e9246d70f0d5c09b910
Daniel Vanko <daaniiieel@disroot.org>
2020-06-07 21:49:24 +0200

    refactor(main architecture): move from telegram-bot-api to telegraf
    this enables the code to be shorter, use a more recent version of the bot API and most importantly
    reduces code size by ~10MB

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 //     _ _                       _       _       _                      
 //  __| (_)___  ___ ___  _ __ __| |     | |_ ___| | ___  __ _ _ __ __ _ 
 // / _` | / __|/ __/ _ \| '__/ _` |_____| __/ _ \ |/ _ \/ _` | '__/ _` |
@@ -10,45 +12,40 @@
 //| | | | | |_____| |_) | |  | | (_| | (_| |  __/
 //|_| |_| |_|     |_.__/|_|  |_|\__,_|\__, |\___|
 //                                    |___/      
-
-#!/usr/bin/env node
+console.clear()
 const fetch = require("node-fetch");
 
 const wakeUpDyno = (url, interval = 25, callback) => {
-    const milliseconds = interval * 60000;
-    setTimeout(() => {
+  const milliseconds = interval * 60000;
+  setTimeout(() => {
 
-        try { 
-            console.log("setTimeout called.");
+    try {
+      console.log("setTimeout called.");
             // HTTP GET request to the dyno's url
-            fetch(url).then(() => console.log(`Fetching ${url}.`)); 
-        }
-        catch (err) { // catch fetch errors
-            console.log(`Error fetching ${url}: ${err.message} 
+            fetch(url).then(() => console.log(`Fetching ${url}.`));
+        } catch (err) { // catch fetch errors
+          console.log(`Error fetching ${url}: ${err.message} 
             Will try again in ${interval} minutes...`);
-        }
-        finally {
+        } finally {
 
-            try {
+          try {
                 callback(); // execute callback, if passed
-            }
-            catch (e) { // catch callback error
-                callback ? console.log("Callback failed: ", e.message) : null;
-            }
-            finally {
+            } catch (e) { // catch callback error
+              callback ? console.log("Callback failed: ", e.message) : null;
+            } finally {
                 // do it all again
                 return wakeUpDyno(url, interval, callback);
-            }
-            
-        }
+              }
 
-    }, milliseconds);
+            }
+
+          }, milliseconds);
 };
 
 // heroku specific
-const express = require("express"); 
+const express = require("express");
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 const DYNO_URL = process.env.DYNO_URL || "https://google.com"; //dyno url
 
 const app = express();
@@ -57,13 +54,12 @@ app.get("/", function(req, res) {
 });
 app.listen(PORT, () => {
     wakeUpDyno(DYNO_URL); // will start once server starts
-});
+  });
 
 // import modules
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var telegram = require("natsvora-telegram-bot-api");
-
+const Telegram = require('telegraf/telegram');
 // import env variables
 var telegramToken = process.env.TELEGRAM_BOT_TOKEN;
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
@@ -73,15 +69,10 @@ var discordChannelId = process.env.DISCORD_CHANNEL_ID;
 const webhookClient = new Discord.WebhookClient(
   process.env.webhook_id,
   process.env.webhook_token
-);
+  );
 // initializes the telegram bot and starts listening for updates (new messages)
-var api = new telegram({
-  token: telegramToken,
-  updates: {
-    enabled: true
-  }
-});
-
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf(telegramToken)
 client.once("ready", () => {
   console.log("Discord bot ready!");
 });
@@ -92,97 +83,83 @@ client.login(DISCORD_TOKEN);
 // if the discord bot receives a message
 client.on("message", message => {
   if (
-    // the program currently check if the message's from a bot to check for duplicates. This isn't the best method but it's good enough. A webhook counts as a bot in the discord api, don't ask me why.
-    message.channel.id == discordChannelId &&
-    message.author.bot === false
-  ) {
-    //console.log(message)
-    let mentionedUsernames = [];
-for(let mention of message.mentions.users){mentionedUsernames.push("@"+mention[1].username);}
-    var attachmentUrls = [];
-    for(let attachment of message.attachments){
-      attachmentUrls.push(attachment[1].url);
-    }
-    // attachmentUrls is empty when there are no attachments so we can be just lazy
-    var finalMessageContent = message.content.replace(/<@.*>/gi, "");
-    //console.log(message.author.username + ": "+finalMessageContent + " "+ attachmentUrls.join(" ") + mentionedUsernames.join(" "))
-    api.sendMessage({
-      chat_id: telegramChatId,
-      text: message.author.username + ": "+finalMessageContent + " "+ attachmentUrls.join(" ") + mentionedUsernames.join(" ")
+        // the program currently check if the message's from a bot to check for duplicates. This isn't the best method but it's good enough. A webhook counts as a bot in the discord api, don't ask me why.
+        message.channel.id == discordChannelId &&
+        message.author.bot === false
+        ) {
+        //console.log(message)
+      let mentionedUsernames = [];
+      for (let mention of message.mentions.users) { mentionedUsernames.push("@" + mention[1].username); }
+        var attachmentUrls = [];
+      for (let attachment of message.attachments) {
+        attachmentUrls.push(attachment[1].url);
+      }
+        // attachmentUrls is empty when there are no attachments so we can be just lazy
+        var finalMessageContent = message.content.replace(/<@.*>/gi, "");
+        //console.log(message.author.username + ": "+finalMessageContent + " "+ attachmentUrls.join(" ") + mentionedUsernames.join(" "))
+        bot.telegram.sendMessage( telegramChatId, message.author.username + ": " + finalMessageContent + " " + attachmentUrls.join(" ") + mentionedUsernames.join(" "));
+
+      }
     });
- 
-  }
-});
 
 var photoUrl = "";
-api.on("message", function(message) {
-  // console.log(message)
+bot.on("message", function(message) {
+const messageUpdate= message.update
+const updateMsg = message.update.message
   var filePath = "";
-  if (message.chat.id == telegramChatId && message.from.is_bot === false) {
+  if (updateMsg.chat.id == telegramChatId && updateMsg.from.is_bot === false) {
         // this part gets the user profile photos as the variable names suggest
         let getProfilePic = new Promise(function(resolve, reject) {
-          var profilePhotos = api.getUserProfilePhotos({ user_id: message.from.id });
+          var profilePhotos = bot.telegram.getUserProfilePhotos(updateMsg.from.id);
           profilePhotos.then(function(data) {
                 // if user has a profile photo
+                // console.log(data)
                 if (data.total_count > 0) {
-                  var file = api.getFile({ file_id: data.photos[0][0].file_id });
+                  var file = bot.telegram.getFileLink(data.photos[0][0].file_id);
                   file.then(function(result) {
-                    var filePath = result.file_path;
-
-                    resolve("https://api.telegram.org/file/bot" + telegramToken + "/" + filePath);
-
+                  resolve(result);
                   });
                 } else {
-                    //console.log("telegram pfp")
                     resolve("https://telegram.org/img/t_logo.png");
                   }
                 });
         });
-        getProfilePic.then(function(profileUrl) { 
-          // if the message contains media
-          if (message.document || message.photo || message.sticker) {
-            if (message.document) {
-              var document = api.getFile({ file_id: message.document.file_id });
-              document.then(function(data) {
-                var documentUrl =
-                  "https://api.telegram.org/file/bot" + telegramToken + "/" +  data.file_path;
-                webhookClient.send(message.caption, {
-                  username: message.from.first_name,
-                  avatarURL: profileUrl,
-                  files: [documentUrl]
+        getProfilePic.then(function(profileUrl) {
+            // if the message contains media
+            if (updateMsg.document || updateMsg.photo || updateMsg.sticker) {
+              bot.telegram.getFileLink(updateMsg.document.file_id).then(function(photoUrl) {
+                  webhookClient.send(updateMsg.caption, {
+                    username: updateMsg.from.first_name,
+                    avatarURL: profileUrl,
+                            files: [photoUrl]
+                        });
+                    });
+              if (updateMsg.sticker) {
+                bot.telegram.getFileLink(updateMsg.sticker.file_id).then(function(photoUrl) {
+                  webhookClient.send(updateMsg.caption, {
+                    username: updateMsg.from.first_name,
+                    avatarURL: profileUrl,
+                            files: [photoUrl]
+                        });
+                    });
+              }
+              if (updateMsg.photo) {
+                bot.telegram.getFileLink(updateMsg.photo[0].file_id).then(function(photoUrl) {
+                  webhookClient.send(updateMsg.caption, {
+                    username: updateMsg.from.first_name,
+                    avatarURL: profileUrl,
+                            files: [photoUrl]
+                        });
+                    });
+                }
+            } else {
+                webhookClient.send(updateMsg.text, {
+                    username: updateMsg.from.first_name,
+                    avatarURL: profileUrl
                 });
-              });
             }
-            if(message.sticker){
-              var sticker = api.getFile({ file_id: message.sticker.file_id });
-              sticker.then(function(data) {
-                var stickerUrl =
-                  "https://api.telegram.org/file/bot" + telegramToken + "/" +  data.file_path;
-                webhookClient.send(message.caption, {
-                  username: message.from.first_name,
-                  avatarURL: profileUrl,
-                  files: [stickerUrl]
-                });
-              });
-          }
-            if (message.photo) {
-              var photo = api.getFile({ file_id: message.photo[0].file_id });
-              photo.then(function(data) {
-                var photoUrl =
-                  "https://api.telegram.org/file/bot" + telegramToken +"/" +data.file_path;
-                webhookClient.send(message.caption, {
-                  username: message.from.first_name,
-                  avatarURL: profileUrl,
-                  files: [photoUrl]
-                });
-              });
-            }
-          } else {
-            webhookClient.send(message.text, {
-              username: message.from.first_name,
-              avatarURL: profileUrl
-            });
-          } });
-         
-      }
-    });
+        });
+
+    }
+});
+bot.launch()
