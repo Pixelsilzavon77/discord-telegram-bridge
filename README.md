4e7d9ad38dd836e3aeba22ef214bda29865c849e
Daniel Vanko <daaniiieel@disroot.org>
2020-06-16 10:25:34 +0200

    style: prettier code

--- README.md
+++ README.md
@@ -2,32 +2,36 @@
 
 ![GitHub top language](https://img.shields.io/github/languages/top/daaniiieel/discord-telegram-bridge?style=for-the-badge) ![Maintenance](https://img.shields.io/maintenance/yes/2020?style=for-the-badge) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/ebfcea5b1b2b4780a7b1054d7760ef8a)](https://app.codacy.com/manual/daaniiieel/discord-telegram-bridge?utm_source=github.com&utm_medium=referral&utm_content=daaniiieel/discord-telegram-bridge&utm_campaign=Badge_Grade_Settings)![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/daaniiieel/discord-telegram-bridge?style=for-the-badge) ![npm](https://img.shields.io/npm/v/discord-telegram-bridge?style=for-the-badge)
 
-A small and fast telegram to discord chat bridge, written in node 
+A small and fast telegram to discord chat bridge, written in node
 
 #### Setup
-> If you want to run it on heroku, you can just click the button below. 
+
+> If you want to run it on heroku, you can just click the button below.
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/filcnaplo/discord-telegram-bridge)
 
-* Execute `npm install -g discord-telegram-bridge --production`
-* Create a Discord webhook. You can do this by going to Server settings -> Webhooks. Copy the URL, you'll need it later.
-* Set env variables: 
+- Execute `npm install -g discord-telegram-bridge --production`
+- Create a Discord webhook. You can do this by going to Server settings -> Webhooks. Copy the URL, you'll need it later.
+- Set env variables:
 
-|Env variable|type|value|
-|-|-|-|
-|`TELEGRAM_BOT_TOKEN`|string|You can get this by speaking with @BotFather on telegram and creating a new bot.|
-|`DISCORD_TOKEN`|string|The bot token for your Discord application. Create a new app at the  Discord Developer Portal , go to the bot section, click on Create a bot and copy the bot token it gives to you.|
-|`TELEGRAM_CHAT_ID`|string|The ChatId of the telegram group you want to bridge.|
-|`DISCORD_CHANNEL_ID`|string|The Discord ChannelId of the channel you want to bridge.|
-|`webhook_id`  and  `webhook_token`|string|Those are part of the webhook URL you copied.  webhook_id  is a 18 characters long int,  webhook_token is a ~70 chars long randomly generated string. Those are seperated by slashes in the url.|
-* Run `discord-telegram-bridge` and you're set!
+| Env variable                     | type   | value                                                                                                                                                                                         |
+| -------------------------------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `TELEGRAM_BOT_TOKEN`             | string | You can get this by speaking with @BotFather on telegram and creating a new bot.                                                                                                              |
+| `DISCORD_TOKEN`                  | string | The bot token for your Discord application. Create a new app at the Discord Developer Portal , go to the bot section, click on Create a bot and copy the bot token it gives to you.           |
+| `TELEGRAM_CHAT_ID`               | string | The ChatId of the telegram group you want to bridge.                                                                                                                                          |
+| `DISCORD_CHANNEL_ID`             | string | The Discord ChannelId of the channel you want to bridge.                                                                                                                                      |
+| `webhook_id` and `webhook_token` | string | Those are part of the webhook URL you copied. webhook_id is a 18 characters long int, webhook_token is a ~70 chars long randomly generated string. Those are seperated by slashes in the url. |
+
+- Run `discord-telegram-bridge` and you're set!
 
 #### Contributing
-> Wondering what to contribute? The project has a codacy page at [https://app.codacy.com/manual/daaniiieel/discord-telegram-bridge/dashboard](https://app.codacy.com/manual/daaniiieel/discord-telegram-bridge/dashboard). On the issues tab, you can see all the code quality (mostly code style) issues we have. 
-* Fork this github repo
-* Clone your fork to your machine
-* Execute `npm install` **without** the `--production` flag
-* Write code
-* Once done, `git add *` then  `git cz`. This uses Commitizen (installed automatically as a devDependency with `npm install`) to write beautiful commit messages
-* Make a pull request from your fork to this repo
-* After reviewing it, your code will be merged
+
+> Wondering what to contribute? The project has a codacy page at [https://app.codacy.com/manual/daaniiieel/discord-telegram-bridge/dashboard](https://app.codacy.com/manual/daaniiieel/discord-telegram-bridge/dashboard). On the issues tab, you can see all the code quality (mostly code style) issues we have.
+
+- Fork this github repo
+- Clone your fork to your machine
+- Execute `npm install` **without** the `--production` flag
+- Write code
+- Once done, `git add *` then `git cz`. This uses Commitizen (installed automatically as a devDependency with `npm install`) to write beautiful commit messages
+- Make a pull request from your fork to this repo
+- After reviewing it, your code will be merged
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-//     _ _                       _       _       _                      
-//  __| (_)___  ___ ___  _ __ __| |     | |_ ___| | ___  __ _ _ __ __ _ 
+//     _ _                       _       _       _
+//  __| (_)___  ___ ___  _ __ __| |     | |_ ___| | ___  __ _ _ __ __ _
 // / _` | / __|/ __/ _ \| '__/ _` |_____| __/ _ \ |/ _ \/ _` | '__/ _` |
 //| (_| | \__ \ (_| (_) | | | (_| |_____| ||  __/ |  __/ (_| | | | (_| |
 // \__,_|_|___/\___\___/|_|  \__,_|      \__\___|_|\___|\__, |_|  \__,_|
-//                                                      |___/           
-//                 _          _     _            
-// _ __ ___       | |__  _ __(_) __| | __ _  ___ 
+//                                                      |___/
+//                 _          _     _
+// _ __ ___       | |__  _ __(_) __| | __ _  ___
 //| '_ ` _ \ _____| '_ \| '__| |/ _` |/ _` |/ _ \
 //| | | | | |_____| |_) | |  | | (_| | (_| |  __/
 //|_| |_| |_|     |_.__/|_|  |_|\__,_|\__, |\___|
-//                                    |___/      
+//                                    |___/
 
 //clear console before start -- useful for debugging, disabled from ver 1.0.1
 //console.clear()
@@ -22,35 +22,33 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 var telegramChatId = process.env.TELEGRAM_CHAT_ID;
 var discordChannelId = process.env.DISCORD_CHANNEL_ID;
 const PORT = process.env.PORT || 3000;
-const DYNO_URL = process.env.DYNO_URL || "https://google.com"; 
+const DYNO_URL = process.env.DYNO_URL || "https://google.com";
 
 const fetch = require("node-fetch");
 
 const wakeUpDyno = (url, interval = 25, callback) => {
-    const milliseconds = interval * 60000;
-    setTimeout(() => {
-
-        try {
-            console.log("setTimeout called.");
-            // HTTP GET request to the dyno's url
-            fetch(url).then(() => console.log(`Fetching ${url}.`));
-        } catch (err) { // catch fetch errors
-            console.log(`Error fetching ${url}: ${err.message} 
+  const milliseconds = interval * 60000;
+  setTimeout(() => {
+    try {
+      console.log("setTimeout called.");
+      // HTTP GET request to the dyno's url
+      fetch(url).then(() => console.log(`Fetching ${url}.`));
+    } catch (err) {
+      // catch fetch errors
+      console.log(`Error fetching ${url}: ${err.message} 
             Will try again in ${interval} minutes...`);
-        } finally {
-
-            try {
-                callback(); // execute callback, if passed
-            } catch (e) { // catch callback error
-                callback ? console.log("Callback failed: ", e.message) : null;
-            } finally {
-                // do it all again
-                return wakeUpDyno(url, interval, callback);
-            }
-
-        }
-
-    }, milliseconds);
+    } finally {
+      try {
+        callback(); // execute callback, if passed
+      } catch (e) {
+        // catch callback error
+        callback ? console.log("Callback failed: ", e.message) : null;
+      } finally {
+        // do it all again
+        return wakeUpDyno(url, interval, callback);
+      }
+    }
+  }, milliseconds);
 };
 
 // import modules
@@ -61,29 +59,32 @@ const polka = require("polka");
 const { Telegraf } = require("telegraf");
 
 polka()
-    .get("/", (req, res) => {
-        res.end("Hello world!");
-    })
-    .listen(PORT, (err) => {
-        if (err){throw err};
-        console.log("> Running on localhost:" + PORT);
-        if (process.env._ && process.env._.indexOf("heroku") !== -1) {
-            console.log("> Starting wakeUpDyno");
-            wakeUpDyno(DYNO_URL);
-        } else { console.log("> Not running on heroku"); }
-    });
-
+  .get("/", (req, res) => {
+    res.end("Hello world!");
+  })
+  .listen(PORT, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("> Running on localhost:" + PORT);
+    if (process.env._ && process.env._.indexOf("heroku") !== -1) {
+      console.log("> Starting wakeUpDyno");
+      wakeUpDyno(DYNO_URL);
+    } else {
+      console.log("> Not running on heroku");
+    }
+  });
 
 const webhookClient = new Discord.WebhookClient(
-    process.env.webhook_id,
-    process.env.webhook_token
+  process.env.webhook_id,
+  process.env.webhook_token
 );
 
 // initializes the telegram bot and starts listening for updates (new messages)
 
-const bot = new Telegraf(telegramToken)
+const bot = new Telegraf(telegramToken);
 client.once("ready", () => {
-    console.log("Discord bot ready!");
+  console.log("Discord bot ready!");
 });
 
 // initializes discord bot
@@ -91,89 +92,99 @@ client.login(DISCORD_TOKEN);
 
 // if the discord bot receives a message
 client.on("message", (message) => {
-    if (
-        // the program currently check if the message's from a bot to check for duplicates. This isn't the best method but it's good enough. A webhook counts as a bot in the discord api, don't ask me why.
-        message.channel.id == discordChannelId &&
-        message.author.bot === false
-    ) {
-        //console.log(message)
-        let mentionedUsernames = [];
-        for (let mention of message.mentions.users) { mentionedUsernames.push("@" + mention[1].username); }
-        var attachmentUrls = [];
-        for (let attachment of message.attachments) {
-            attachmentUrls.push(attachment[1].url);
-        }
-        // attachmentUrls is empty when there are no attachments so we can be just lazy
-        var finalMessageContent = message.content.replace(/<@.*>/gi, "");
-        //console.log(message.author.username + ": "+finalMessageContent + " "+ attachmentUrls.join(" ") + mentionedUsernames.join(" "))
-        bot.telegram.sendMessage(telegramChatId, message.author.username + ": " + finalMessageContent + " " + attachmentUrls.join(" ") + mentionedUsernames.join(" "));
-
+  if (
+    // the program currently check if the message's from a bot to check for duplicates. This isn't the best method but it's good enough. A webhook counts as a bot in the discord api, don't ask me why.
+    message.channel.id == discordChannelId &&
+    message.author.bot === false
+  ) {
+    let mentionedUsernames = [];
+    for (let mention of message.mentions.users) {
+      mentionedUsernames.push("@" + mention[1].username);
     }
+    var attachmentUrls = [];
+    for (let attachment of message.attachments) {
+      attachmentUrls.push(attachment[1].url);
+    }
+    // attachmentUrls is empty when there are no attachments so we can be just lazy
+    var finalMessageContent = message.content.replace(/<@.*>/gi, "");
+    bot.telegram.sendMessage(
+      telegramChatId,
+      message.author.username +
+        ": " +
+        finalMessageContent +
+        " " +
+        attachmentUrls.join(" ") +
+        mentionedUsernames.join(" ")
+    );
+  }
 });
 
 var photoUrl = "";
-bot.on("message", function(message) {
-    const messageUpdate = message.update;
-    const updateMsg = message.update.message;
-    var filePath = "";
-    if (updateMsg.chat.id == telegramChatId && updateMsg.from.is_bot === false) {
-        // this part gets the user profile photos as the variable names suggest
-        let getProfilePic = new Promise(function(resolve, reject) {
-            var profilePhotos = bot.telegram.getUserProfilePhotos(updateMsg.from.id);
-            profilePhotos.then(function(data) {
-                // if user has a profile photo
-                // console.log(data)
-                if (data.total_count > 0) {
-                    var file = bot.telegram.getFileLink(data.photos[0][0].file_id);
-                    file.then(function(result) {
-                        resolve(result);
-                    });
-                } else {
-                    resolve("https://telegram.org/img/t_logo.png");
-                }
+bot.on("message", function (message) {
+  const messageUpdate = message.update;
+  const updateMsg = message.update.message;
+  var filePath = "";
+  if (updateMsg.chat.id == telegramChatId && updateMsg.from.is_bot === false) {
+    // this part gets the user profile photos as the variable names suggest
+    let getProfilePic = new Promise(function (resolve, reject) {
+      var profilePhotos = bot.telegram.getUserProfilePhotos(updateMsg.from.id);
+      profilePhotos.then(function (data) {
+        // if user has a profile photo
+        // console.log(data)
+        if (data.total_count > 0) {
+          var file = bot.telegram.getFileLink(data.photos[0][0].file_id);
+          file.then(function (result) {
+            resolve(result);
+          });
+        } else {
+          resolve("https://telegram.org/img/t_logo.png");
+        }
+      });
+    });
+    getProfilePic.then(function (profileUrl) {
+      // if the message contains media
+      if (updateMsg.document || updateMsg.photo || updateMsg.sticker) {
+        if (updateMsg.document) {
+          bot.telegram
+            .getFileLink(updateMsg.document.file_id)
+            .then(function (photoUrl) {
+              webhookClient.send(updateMsg.caption, {
+                username: updateMsg.from.first_name,
+                avatarURL: profileUrl,
+                files: [photoUrl],
+              });
             });
-        });
-        getProfilePic.then(function(profileUrl) {
-            // if the message contains media
-            if (updateMsg.document || updateMsg.photo || updateMsg.sticker) {
-                if (updateMsg.document) {
-                    console.log("❯ MESSAGE CONTAINS DOCUMENT")
-                    bot.telegram.getFileLink(updateMsg.document.file_id).then(function(photoUrl) {
-                        webhookClient.send(updateMsg.caption, {
-                            username: updateMsg.from.first_name,
-                            avatarURL: profileUrl,
-                            files: [photoUrl]
-                        });
-                    });
-                }
-               
-                if (updateMsg.sticker) {
-                    bot.telegram.getFileLink(updateMsg.sticker.file_id).then(function(photoUrl) {
-                        webhookClient.send(updateMsg.caption, {
-                            username: updateMsg.from.first_name,
-                            avatarURL: profileUrl,
-                            files: [photoUrl]
-                        });
-                    });
-                }
-                if (updateMsg.photo) {
-                    console.log("❯ MESSAGE CONTAINS PHOTO")
-                    bot.telegram.getFileLink(updateMsg.photo[0].file_id).then(function(photoUrl) {
-                        webhookClient.send(updateMsg.caption, {
-                            username: updateMsg.from.first_name,
-                            avatarURL: profileUrl,
-                            files: [photoUrl]
-                        });
-                    });
-                }
-            } else {
-                webhookClient.send(updateMsg.text, {
-                    username: updateMsg.from.first_name,
-                    avatarURL: profileUrl
-                });
-            }
-        });
+        }
 
-    }
+        if (updateMsg.sticker) {
+          bot.telegram
+            .getFileLink(updateMsg.sticker.file_id)
+            .then(function (photoUrl) {
+              webhookClient.send(updateMsg.caption, {
+                username: updateMsg.from.first_name,
+                avatarURL: profileUrl,
+                files: [photoUrl],
+              });
+            });
+        }
+        if (updateMsg.photo) {
+          bot.telegram
+            .getFileLink(updateMsg.photo[0].file_id)
+            .then(function (photoUrl) {
+              webhookClient.send(updateMsg.caption, {
+                username: updateMsg.from.first_name,
+                avatarURL: profileUrl,
+                files: [photoUrl],
+              });
+            });
+        }
+      } else {
+        webhookClient.send(updateMsg.text, {
+          username: updateMsg.from.first_name,
+          avatarURL: profileUrl,
+        });
+      }
+    });
+  }
 });
 bot.launch();
